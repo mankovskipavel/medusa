915218d4eee9ce3e5299d47646e2c8cefdc2608c
MankovskiPavel <mankovskipavel@gmail.com>
2021-10-03 23:29:28 +0300

    fix path

--- pages/3d print/script.js
+++ pages/3d print/script.js
@@ -1,16 +1,15 @@
-const sliderLeft = document.querySelector('.arrow-container_left');
-const sliderRight = document.querySelector('.arrow-container_right');
-const sliderImg =  document.getElementById('sliderImg');
+const sliderLeft = document.querySelector(".arrow-container_left");
+const sliderRight = document.querySelector(".arrow-container_right");
+const sliderImg = document.getElementById("sliderImg");
 let iterator = 1;
 const firstSlide = 1;
 const lastSlide = 9;
-sliderLeft.addEventListener('click', ()=>{
-    iterator === firstSlide ? iterator = lastSlide :  iterator--;
-    sliderImg.src='../../../images/' + iterator.toString() + '.JPG';   
-    
-})
+sliderLeft.addEventListener("click", () => {
+    iterator === firstSlide ? (iterator = lastSlide) : iterator--;
+    sliderImg.src = "../../images/" + iterator.toString() + ".JPG";
+});
 
-sliderRight.addEventListener('click', ()=>{
-    iterator === lastSlide ? iterator = firstSlide : iterator++;
-    sliderImg.src='../../../images/' + iterator.toString() + '.JPG';       
-})
+sliderRight.addEventListener("click", () => {
+    iterator === lastSlide ? (iterator = firstSlide) : iterator++;
+    sliderImg.src = "../../images/" + iterator.toString() + ".JPG";
+});
